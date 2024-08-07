b97a99e55346ae38bcc7f4a8f4676e4b3ff155dd
NickRayan <nickcerron@gmail.com>
2024-08-07 13:52:30 -0500

    pdfmakertools: cm to points reformated

--- src/main/java/com/image_tools/dev/image_tools/services/PdfMakerTools.java
+++ src/main/java/com/image_tools/dev/image_tools/services/PdfMakerTools.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class PdfMakerTools {
   private static final float CM_TO_POINTS = 72f / 2.54f;
+
   public float cmToPoints(float cm) {
-    return cm * CM_TO_POINTS;
+    float points =  Math.round(cm * CM_TO_POINTS * 100) / 100f;
+    return points;
   }
 }
